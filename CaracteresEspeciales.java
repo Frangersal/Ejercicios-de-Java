e2bb8a40cc6ec9e3c95c5888bfbc76ad6c4d4617
Francisco G. Salinas <frangersal@outlook.com>
2026-05-26 09:11:28 -0600

    1.4 Carateres especiales

---
+++ CaracteresEspeciales.java
@@ -0,0 +1,6 @@
+public class CaracteresEspeciales {
+    public static void main(String[] args) {
+        System.out.println("Cuando programo ⌨ estoy");
+        System.out.println(" muy ☺ y me siento como un ♔.");
+    }
+}
