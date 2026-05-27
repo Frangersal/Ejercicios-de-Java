4410ca4d0fa32789a3252b0462a9831ad1c133b1
Francisco G. Salinas <frangersal@outlook.com>
2026-05-27 13:03:19 -0600

    1.5 Salida formateada 01

---
+++ SalidaFormateada01.java
@@ -0,0 +1,7 @@
+public class SalidaFormateada01 {
+    public static void main(String[] args) {
+        System.out.printf("El número %d no tienes decimales. \n", 21);
+        System.out.printf("El número %f no tienes decimales. \n", 21.0);
+        System.out.printf("El %.3f sale exactamente con 3 decimales. \n", 21.0);
+    }
+}
